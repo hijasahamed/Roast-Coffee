1bc0f9548e9f96a4d0cdc006397412080ad6c51b
hijas <hijasahamed2000@gmail.com>
2025-02-01 15:16:22 +0530

    feat: profile screen wallet balance and adress card added

--- lib/view/screens/profile_screen.dart
+++ lib/view/screens/profile_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:roast_coffee/view/widgets/profile_screen_widgets/profile_app_bar_widget.dart';
+import 'package:roast_coffee/view/widgets/profile_screen_widgets/wallet_address_widget.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key,required this.screenSize});
@@ -8,10 +9,25 @@ class ProfileScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0XFFF5F5F5),
-      extendBodyBehindAppBar: true,
+      extendBodyBehindAppBar: false,
       appBar: AppBar(
         toolbarHeight: screenSize.height/4,
         flexibleSpace: ProfileAppbarWidget(screenSize: screenSize),
+      ),
+      body: Container(
+        margin: EdgeInsets.only(left: screenSize.width/20,right: screenSize.width/20),
+        padding: EdgeInsets.only(left: screenSize.width/17,right: screenSize.width/17,top: screenSize.width/11.5,bottom: screenSize.width/15),
+        height: screenSize.height,
+        width: screenSize.width,
+        color: Color(0XFFFFFFFF),
+        child: Column(
+          children: [
+            WalletAddressWidget(image: 'assets/images/tabler-icon-wallet blue.png', subtitle: 'BHD 86.000', title: 'Wallet Balance', screenSize: screenSize,cardnum: 1,),
+            Divider(color: Color(0XFF6E8382),thickness: .3,),
+            WalletAddressWidget(image: 'assets/images/tabler-icon-address-book blue.png', subtitle: '107, Omar Bin Abdulaziz Avenue Al Hoora, Manama, Capital Governorate, Bahrain, 0319.', title: 'Address', screenSize: screenSize,cardnum: 2,),
+            Divider(color: Color(0XFF6E8382),thickness: .3,),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/view/widgets/profile_screen_widgets/wallet_address_widget.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:roast_coffee/view/widgets/common_widgets/text_widget.dart';
+
+class WalletAddressWidget extends StatelessWidget {
+  const WalletAddressWidget({
+    super.key,
+    required this.image,
+    required this.subtitle,
+    required this.title,
+    required this.screenSize,
+    required this.cardnum,
+  });
+
+  final String image;
+  final String title;
+  final String subtitle;
+  final Size screenSize;
+  final int cardnum;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(top: screenSize.width/80,bottom: screenSize.width/60 ),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Image.asset(
+            image,
+            fit: BoxFit.cover,
+            height: screenSize.width / 12,
+            width: screenSize.width / 12,
+          ),
+          SizedBox(width: screenSize.width * 0.035),
+          Expanded( 
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                TextWidget(
+                  text: title,
+                  color: Color(0XFF356B69),
+                  size: screenSize.width * 0.036,
+                  weight: FontWeight.w500,
+                ),
+                SizedBox(height: screenSize.height * 0.003),
+                TextWidget(
+                  text: subtitle,
+                  color: Color(0XFF356B69),
+                  size: cardnum == 2
+                      ? screenSize.width * 0.032
+                      : screenSize.width * 0.045,
+                  weight: cardnum == 2 ? FontWeight.w100 : FontWeight.w600,
+                  maxline: true,
+                ),
+              ],
+            ),
+          ),
+          Container(
+            height: screenSize.width / 18,
+            width: screenSize.width / 18,
+            decoration: BoxDecoration(
+              border: Border.all(color: Color(0XFFCEAC6D), width: 1.5),
+              borderRadius: BorderRadius.circular(screenSize.width / 45),
+            ),
+            child: Center(
+              child: Icon(
+                Icons.add,
+                size: screenSize.width/30,
+                color: Color(0XFFCEAC6D),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
